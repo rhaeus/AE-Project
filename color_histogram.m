cfe9a858cf13252f4372d210effae486bd852763
Ramona Häuselmann <ramona.haeuselmann@gmail.com>
2023-01-10 11:01:55 +0100

    color histogram offset 1 to limit inverse

--- color_histogram.m
+++ color_histogram.m
@@ -2,7 +2,10 @@ function histogram = color_histogram(frame, colour)
     [H, W, D] = size(frame); %height, width, dimension of video frame matrix
     RGB_matrix = double(reshape(frame,[H*W, D])); % create matrix with R G B values listed in separate columns
     histogram = pdist2(RGB_matrix, colour, "euclidean");
+    histogram = histogram + 1;
+    
     histogram = 1./histogram ; 
+%     histogram = histogram .^ 2;
     % histogram = histogram / sum(sum(histogram)) ;
     
     histogram = reshape(histogram, H, W);
